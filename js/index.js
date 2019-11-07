149592e4cdcdcba6ce00a48567b57d237a352421
denis <denis.doynikov@gmail.com>
2019-11-07 18:51:28 +0300

    07.11 work

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 import "./import/modules";
+import "./import/slick";
+import $ from "jquery";
+
+
+$('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
